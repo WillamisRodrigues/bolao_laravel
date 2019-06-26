212a5323123d3cdb67311596e2bcedc8610f9994
WillamisRodrigues <wrodrigues153@gmail.com>
2019-06-26 00:11:27 -0300

    atualização testes bolao php

--- index.php
+++ index.php
@@ -51,3 +51,50 @@
     public $placarTimeA; // placar time A
     public $placarTimeB; // placar time B
    }
+
+// testes:
+   // Gerenciador do Bolão
+   $admin = new Usuario;
+   $admin->id = 1;
+   $admin->nome = "Admin";
+   $admin->email = "admin@mail.com";
+   $admin->senha = "123456";
+
+
+   // Apostador do Bolão
+   $apostador = new Usuario;
+   $apostador->id = 2;
+   $apostador->nome = "Apostador";
+   $apostador->email = "apostador@mail.com";
+   $apostador->senha = "123456";
+
+   // Bolão
+   $bolao = new Bolao; 
+   $bolao->$id = 1;
+   $bolao->$usuario_id = $admin->id;
+   $bolao->$titulo = "Bolão 1";
+   $bolao->$rodadaAtual = 0;
+   $bolao->$pontosResultado = 10;
+   $bolao->$pontosExtras = 5;
+   $bolao->$pontosTaxa = 5;
+
+   // Rodada Bolão
+   $rodada = new Rodada;
+   $rodada->id = 1;
+   $rodada->bolao_id = $bolao_id;
+   $rodada->titulo = "Brasil x Alemanha";
+   $rodada->dataIni = "2019-06-26"; 
+   $rodada->dataFim = "2019-06-28";
+
+   //Partida
+   $partida1 = new Partida;
+   $partida1->id;
+   $partida1->rodada_id;
+   $partida1->titulo;
+   $partida1->estadio;
+   $partida1->timeA;
+   $partida1->timeB;
+   $partida1->resultado; // (A,B,E) E = Empate
+   $partida1->placarTimeA; // resultado time A
+   $partida1->placarTimeB; // resultado time B
+   $partida1->data;
